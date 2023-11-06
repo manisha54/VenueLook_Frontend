6b678197fda305adecf98b969bd575eaec5553ff
manisha54 <manishachaudhary2057@gmail.com>
2023-11-06 20:29:11 +0545

    user login connect with api of venuelook

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,3 +1,28 @@
 import axios from "axios"
 
 const baseUrl = 'http://localhost:3007/users/'
+
+
+
+const login = (credentials) => {
+    return axios.post(`${baseUrl}/login`, credentials)
+}
+
+
+const getToken = () => `bearer ${window.localStorage.getItem('token')}`
+
+const getUserInfo = () => {
+    return axios.get(`${baseUrl}/userinfo`, {
+        headers: { Authorization: getToken() }
+    })
+}
+
+const updateUserInfo = (userInfo) => {
+    return axios.put(`${baseUrl}/userinfo`, userInfo, {
+        headers: { Authorization: getToken() }
+    })
+};
+const userService = {
+    register, login, getUserInfo,updateUserInfo
+}
+export default userService 
