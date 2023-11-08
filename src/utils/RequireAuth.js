ac09db40588179532181daa8591e13fb2fd29fa0
manisha54 <manishachaudhary2057@gmail.com>
2023-11-08 16:27:45 +0545

    requirauth created

---
+++ src/utils/RequireAuth.js
@@ -0,0 +1,13 @@
+import { Navigate } from "react-router-dom"
+import { useAuth } from "./authContext"
+
+
+export const RequireAuth = ({ children }) => {
+    const auth = useAuth()
+    if (!auth.email) {
+        return <Navigate to={'/'} />
+    }
+
+
+    return children
+}
