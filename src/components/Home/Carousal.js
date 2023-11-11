912c0ec8e4dd3e4fd292d5358a648e291a6e6f39
manisha54 <manishachaudhary2057@gmail.com>
2023-11-11 13:10:04 +0545

    carousal created

---
+++ src/components/Home/Carousal.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+const Carousel = ({ images }) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const nextSlide = () => {
+    setActiveIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
+  };
+
+  const prevSlide = () => {
+    setActiveIndex((prevIndex) => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
+  };
+
+  return (
+    <div className="carousel" data-testid="carousel">
+      <div className="slides" style={{ transform: `translateX(-${activeIndex * 100}%)` }}>
+        {images.map((image, index) => (
+          <div key={index} className="slide">
+            <img src={image} alt={`Slide ${index + 1}`} />
+          </div>
+        ))}
+      </div>
+      <button className="prev-button" onClick={prevSlide}>
+        &lt;
+      </button>
+      <button className="next-button" onClick={nextSlide}>
+        &gt;
+      </button>
+    </div>
+  );
+};
+
+export default Carousel;
