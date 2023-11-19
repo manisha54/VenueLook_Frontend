29de90e2548762f3ef449341d14415bca0689fb9
manisha54 <manishachaudhary2057@gmail.com>
2023-11-20 02:39:10 +0545

    rendering aboutus page

---
+++ src/components/aboutus.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from './Home/NavBar'
+import Footer from './Home/footer';
+import AboutUs from './Home/AboutUs';
+export default function About() {
+  return (
+    <div>
+      <NavBar/>
+      <AboutUs/>
+      <Footer/>
+    </div>
+  )
+}
