54201ff71d1f3d28a26a8f090d8b7e00b6b64bc6
manisha54 <manishachaudhary2057@gmail.com>
2023-11-23 21:02:54 +0545

    rendring of contactus page

---
+++ src/components/contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from './Home/NavBar'
+import Footer from './Home/footer';
+import ContactUs from './Contact/contactus';
+export default function Contact() {
+  return (
+    <div>
+      <NavBar></NavBar>
+      <ContactUs></ContactUs>
+      <Footer></Footer>
+    </div>
+  )
+}
